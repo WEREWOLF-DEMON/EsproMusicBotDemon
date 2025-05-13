3e0d8853051171e082a9e3fa77ad690663c348e9
anujjjjjjjjjj <dharamrajput2004@gmail.com>
2025-05-13 12:50:13 +0530

    Update stats.py

--- EsproMusic/plugins/tools/stats.py
+++ EsproMusic/plugins/tools/stats.py
@@ -9,13 +9,13 @@ from pyrogram.types import InputMediaPhoto, Message
 from pytgcalls.__version__ import __version__ as pytgver
 
 import config
-from EsproMusic import app
-from EsproMusic.core.userbot import assistants
-from EsproMusic.misc import SUDOERS, mongodb
-from EsproMusic.plugins import ALL_MODULES
-from EsproMusic.utils.database import get_served_chats, get_served_users, get_sudoers
-from EsproMusic.utils.decorators.language import language, languageCB
-from EsproMusic.utils.inline.stats import back_stats_buttons, stats_buttons
+from BrandrdXMusic import app
+from BrandrdXMusic.core.userbot import assistants
+from BrandrdXMusic.misc import SUDOERS, mongodb
+from BrandrdXMusic.plugins import ALL_MODULES
+from BrandrdXMusic.utils.database import get_served_chats, get_served_users, get_sudoers, get_queries
+from BrandrdXMusic.utils.decorators.language import language, languageCB
+from BrandrdXMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
@@ -52,12 +52,14 @@ async def overall_stats(client, CallbackQuery, _):
     await CallbackQuery.edit_message_text(_["gstats_1"].format(app.mention))
     served_chats = len(await get_served_chats())
     served_users = len(await get_served_users())
+    total_queries = await get_queries()
     text = _["gstats_3"].format(
         app.mention,
         len(assistants),
         len(BANNED_USERS),
         served_chats,
         served_users,
+        total_queries,
         len(ALL_MODULES),
         len(SUDOERS),
         config.AUTO_LEAVING_ASSISTANT,
